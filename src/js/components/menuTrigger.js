3ea83b45723647e250bbd4c6d635e09b166290e2
Bohdan Ponamariov <bodiakpi1992@gmail.comm>
2022-08-11 23:05:23 +0300

    fix footer

--- src/js/components/menuTrigger.js
+++ src/js/components/menuTrigger.js
@@ -1,13 +1,22 @@
-let $menuTrigger;
+const menuTrigger = () => {
+	const cardBody = document.body;
+	const planItem = document.querySelector('.menuTrigger');
 
-$menuTrigger = $('.menuTrigger');
-
-$menuTrigger.on('click', () => {
-	if ($('body').hasClass('body--open_menu_state')) {
-		$('body').removeClass('body--open_menu_state');
-		$('.menuTrigger').removeClass('active_mod');
-	} else {
-		$('body').addClass('body--open_menu_state');
-		$('.menuTrigger').addClass('active_mod');
+	function addClass(target) {
+		if (target.closest('.header__body')) {
+			planItem.classList.add('active_mod');
+			cardBody.classList.add('body--open_menu_state');
+		}
 	}
-});
+	document.addEventListener('click', (e) => {
+		const targetElement = e.target;
+		if (cardBody.classList.contains('body--open_menu_state') && targetElement.closest('.menuTrigger')) {
+			cardBody.classList.remove('body--open_menu_state');
+			planItem.classList.remove('active_mod');
+		} else {
+			addClass(targetElement);
+		}
+	});
+};
+
+export default menuTrigger;
