af14e0004de82fc98d80077f571fdef638330343
Bohdan Ponamariov <bodiakpi1992@gmail.comm>
2022-08-15 15:21:19 +0300

    fix bp

--- src/js/components/menuTrigger.js
+++ src/js/components/menuTrigger.js
@@ -1,18 +1,18 @@
 const menuTrigger = () => {
-	const cardBody = document.body;
-	const planItem = document.querySelector('.menuTrigger');
+	const $cardBody = document.body;
+	const $planItem = document.querySelector('.menuTrigger');
 
 	function addClass(target) {
 		if (target.closest('.header__body') && target.closest('.menuTrigger')) {
-			planItem.classList.add('menuTrigger--active_mod');
-			cardBody.classList.add('body--open_menu_state');
+			$planItem.classList.add('menuTrigger--active_mod');
+			$cardBody.classList.add('body--open_menu_state');
 		}
 	}
 	document.addEventListener('click', (e) => {
 		const targetElement = e.target;
-		if (cardBody.classList.contains('body--open_menu_state') && targetElement.closest('.menuTrigger')) {
-			cardBody.classList.remove('body--open_menu_state');
-			planItem.classList.remove('menuTrigger--active_mod');
+		if ($cardBody.classList.contains('body--open_menu_state') && targetElement.closest('.menuTrigger')) {
+			$cardBody.classList.remove('body--open_menu_state');
+			$planItem.classList.remove('menuTrigger--active_mod');
 		} else {
 			addClass(targetElement);
 		}
